fd2b9ed4312cd8c5a23529ea3ae55af89a5b7af7
Mirza Shakil <mirzashakil3jan@gmail.com>
2025-11-21 21:55:04 +0600

    style add

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-function openLogin(){
-document.getElementById("modal").style.display="flex";
-}
-
-
-function closeLogin(){
-document.getElementById("modal").style.display="none";
-}
-
-
-function handleLogin(e){
-e.preventDefault();
-const user=document.getElementById("email").value;
-alert(user + " logged in successfully (demo)");
-closeLogin();
-}
-
-
-function scrollToContact(){
-document.getElementById("contact").scrollIntoView({behavior:"smooth"});
-}
+function openLogin(){document.getElementById("modal").style.display="flex";}
+function closeLogin(){document.getElementById("modal").style.display="none";}
+function handleLogin(e){e.preventDefault();const user=document.getElementById("email").value;alert(user+" logged in successfully (demo)");closeLogin();}
+function scrollToContact(){document.getElementById("contact").scrollIntoView({behavior:"smooth"});}
